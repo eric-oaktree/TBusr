26e4f4e9d30eb30db9c3a5eee881d6b2485a77a9
oakea <erkoak@gmail.com>
2017-02-25 13:58:33 -0500

    io first draft

--- main.py
+++ main.py
@@ -4,6 +4,27 @@
 import crawler
 from datetime import datetime
 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#upper red
+GPIO.setup(17,GPIO.OUT)
+#upper yellow
+GPIO.setup(22,GPIO.OUT)
+#upper green
+GPIO.setup(27,GPIO.OUT)
+
+#turn all on and off
+GPIO.output(17,GPIO.HIGH)
+GPIO.output(22,GPIO.HIGH)
+GPIO.output(27,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(17,GPIO.LOW)
+GPIO.output(22,GPIO.LOW)
+GPIO.output(27,GPIO.LOW)
+
 def mode():
     #toggles mode based on time and day
     now = datetime.today()
@@ -14,3 +35,59 @@ def mode():
     else:
         mode = 2
     return mode
+
+def light(nBus, aBus):
+    if nBus <= 5:
+        GPIO.output(17,GPIO.HIGH)
+        GPIO.output(22,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
+        time.sleep(60)
+    elif nBus <= 10:
+        time.sleep(60)
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(22,GPIO.HIGH)
+        GPIO.output(27,GPIO.LOW)
+    elif nBus > 10:
+        time.sleep(60)
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(22,GPIO.LOW)
+        GPIO.output(27,GPIO.HIGH)
+    else:
+        t = 0
+        while t < 60:
+            GPIO.output(17,GPIO.HIGH)
+            GPIO.output(22,GPIO.LOW)
+            GPIO.output(27,GPIO.HIGH)
+            time.sleep(1)
+            t = t + 1
+            GPIO.output(17,GPIO.LOW)
+            GPIO.output(22,GPIO.HIGH)
+            GPIO.output(27,GPIO.LOW)
+            t = t + 1
+
+def main():
+    if mode() == 1:
+        crawler.grabber('00977')
+        buses = crawler.next_bus()
+        bus = buses['501']
+        nBus = bus['next']
+        aBus = bus['after']
+        nBus = int(nBus) / 60
+        aBus = int(aBus) / 60
+        light(nBus, aBus)
+
+    elif mode () == 2:
+        crawler.grabber('00914')
+        buses = crawler.next_bus()
+        bus = buses['57']
+        nBus = bus['next']
+        aBus = bus['after']
+        nBus = int(nBus) / 60
+        aBus = int(aBus) / 60
+        light(nBus, aBus)
+
+    else:
+        print('Error')
+
+while True:
+    main()
