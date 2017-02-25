00eeeb4ce34af275c07e8db92df3b4ebcd814a88
oakea <erkoak@gmail.com>
2017-02-24 20:06:26 -0500

    notes

--- crawler.py
+++ crawler.py
@@ -1,1 +1,16 @@
 ##T-Tracker
+#<agency tag="mbta" title="MBTA" regionTitle="Massachusetts"/>
+#<route tag="501" title="501"/>
+#501outbound <stop tag="914" title="Washington St opp Montfern Ave" lat="42.3499599" lon="-71.1648699" stopId="00914"/>
+#501inbound <stop tag="977" title="Washington St @ Montfern Ave" lat="42.3498999" lon="-71.16452" stopId="00977"/>
+
+#<route tag="503" title="503"/>
+#503outbound <stop tag="914" title="Washington St opp Montfern Ave" lat="42.3499599" lon="-71.1648699" stopId="00914"/>
+#503inbound <stop tag="977" title="Washington St @ Montfern Ave" lat="42.3498999" lon="-71.16452" stopId="00977"/>
+
+#<route tag="57" title="57"/>
+#57outbound <stop tag="977" title="Washington St @ Montfern Ave" lat="42.3499" lon="-71.16452" stopId="00977"/>
+#57inbound <stop tag="914" title="Washington St opp Montfern Ave" lat="42.3499599" lon="-71.1648699" stopId="00914"/>
+
+
+#http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mbta&stopId=00977
