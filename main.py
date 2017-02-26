dc19e23e8a0bce2fd5b785214a7e620460d3c2ed
Oak <erkoak@gmail.com>
2017-02-26 14:59:50 -0500

    rewrote into functions

--- main.py
+++ main.py
@@ -40,115 +40,115 @@ def mode():
         mode = 2
     return mode
 
+#red = 13 yellow = 6 green = 27
+def convert(color):
+    if color == 'red':
+        return 13
+    elif color == 'yellow':
+        return 6
+    elif color == 'green':
+        return 27
+
+def on(color):
+    pin = convert(color)
+    GPIO.output(pin,GPIO.HIGH)
+
+def off(color):
+    pin = convert(color)
+    GPIO.output(pin,GPIO.LOW)
+
+def blink(color):
+    pin = convert(color)
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(pin,GPIO.LOW)
+    time.sleep(1)
+
+def cont(red, yellow, green):
+    t = 0
+    if red == 'on' and yellow == 'off' and green == 'blink':
+        on(red)
+        off(yellow)
+        while t < 30:
+            blink(green)
+            t = t + 1
+    if red == 'on' and yellow == 'blink' and green == 'off':
+        on(red)
+        off(green)
+        while t < 30:
+            blink(yellow)
+            t = t + 1
+    if red == 'blink' and yellow == 'off' and green == 'off':
+        off(yellow)
+        off(green)
+        while t < 30:
+            blink(red)
+            t = t + 1
+    if red == 'off' and yellow == 'on' and green == 'blink':
+        on(yellow)
+        off(red)
+        while t < 30:
+            blink(green)
+            t = t + 1
+    if red == 'blink' and yellow == 'on' and green == 'off':
+        on(yellow)
+        off(green)
+        while t < 30:
+            blink(red)
+            t = t + 1
+    if red == 'off' and yellow == 'blink' and green == 'off':
+        off(red)
+        off(green)
+        while t < 30:
+            blink(yellow)
+            t = t + 1
+    if red == 'blink' and yellow == 'off' and green == 'on':
+        on(green)
+        off(yellow)
+        while t < 30:
+            blink(red)
+            t = t + 1
+    if red == 'off' and yellow == 'blink' and green == 'on':
+        on(green)
+        off(red)
+        while t < 30:
+            blink(yellow)
+            t = t + 1
+    if red == 'off' and yellow == 'off' and green == 'blink':
+        off(red)
+        off(yellow)
+        while t < 30:
+            blink(green)
+            t = t + 1
+
 def light(nBus, aBus):
     #next bus is under 5 and after bus is under 5
     if nBus <= 5 and aBus <= 5:
-        GPIO.output(13,GPIO.HIGH)
-        GPIO.output(6,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(27,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(27,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('on', 'off', 'blink')
     #next bus is under 5 and after bus is under 10
     elif nBus <= 5 and aBus <= 10:
-        GPIO.output(13,GPIO.HIGH)
-        GPIO.output(27,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(6,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(6,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('on', 'blink', 'off')
     #next bus is under 5 and after bus is over 10
     elif nBus <= 5 and aBus > 10:
-        GPIO.output(6,GPIO.LOW)
-        GPIO.output(27,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(13,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(13,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('blink', 'off', 'off')
     #next bus is under 10 and after bus is under 5
     elif nBus <= 10 and aBus <= 5:
-        GPIO.output(13,GPIO.LOW)
-        GPIO.output(6,GPIO.HIGH)
-        t = 0
-        while t < 60:
-            GPIO.output(27,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(27,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('off', 'on', 'blink')
     #next bus is under 10  and after bus is under 10
     elif nBus <= 10 and aBus <= 10:
-        GPIO.output(13,GPIO.LOW)
-        GPIO.output(27,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(6,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(6,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('off', 'blink', 'off')
     #next bus is under 10 and after bus is over 10
     elif nBus <= 10 and aBus > 10:
-        GPIO.output(27,GPIO.LOW)
-        GPIO.output(6,GPIO.HIGH)
-        t = 0
-        while t < 60:
-            GPIO.output(13,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(13,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('blink', 'on', 'off')
     #next bus is over 10 and after bus is under 5
     elif nBus > 10 and aBus <= 5:
-        GPIO.output(13,GPIO.LOW)
-        GPIO.output(6,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(27,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(27,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('off', 'off', 'blink')
     #next bus is over 10 and after bus is under 10
     elif nBus > 10 and aBus <= 10:
-        GPIO.output(27,GPIO.HIGH)
-        GPIO.output(13,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(6,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(6,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('off', 'blink', 'on')
     #next bus is over 10 and after bus is over 10
     elif nBus > 10 and aBus > 10:
-        GPIO.output(27,GPIO.HIGH)
-        GPIO.output(6,GPIO.LOW)
-        t = 0
-        while t < 60:
-            GPIO.output(13,GPIO.HIGH)
-            time.sleep(1)
-            t = t + 1
-            GPIO.output(13,GPIO.LOW)
-            time.sleep(1)
-            t = t + 1
+        cont('blink', 'off', 'on')
     #errors
     elif nBus == 'er' and aBus == 'er':
         t = 0
@@ -180,9 +180,7 @@ def main():
             nBus = 'er'
             aBus = 'er'
 
-
-
-    elif mode () == 2:
+    elif mode() == 2:
         crawler.grabber('00914')
         buses = crawler.next_bus()
         bus = buses['57']
